a559d6e6ec6b4c03dc2794aaf94713447400660b
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-01-31 18:03:02 +0900

    Included reducer cases for fetched repositories, throttle and keeywords. Included repositories_ids and status in state

--- src/redux/reducers/ui.ts
+++ src/redux/reducers/ui.ts
@@ -1,17 +1,56 @@
 const initial_state: StateUi = {
 	repositories_found: 0,
+	repositories_ids: [],
 	keywords: "",
 	language: null,
 	last_update: null,
 	min_stars: null,
-	page: 1
+	page: 1,
+	status: "characters_0"
 };
 
-export const reducer = (
-	state = initial_state,
-	action: Actions | ActionsWithPayload
-): StateUi => {
+export const reducer = (state = initial_state, action: Actions): StateUi => {
 	switch (action.type) {
+		case "THROTTLE_STARTED": {
+			if (action.payload === "FETCH_REPOSITORIES") {
+				return { ...state, status: "waiting" };
+			}
+
+			return state;
+		}
+
+		case "FETCH_REPOSITORIES": {
+			return { ...state, status: "loading" };
+		}
+
+		case "FETCH_REPOSITORIES_COMPLETED": {
+			return { ...state, status: null };
+		}
+
+		case "FETCH_REPOSITORIES_FAILED": {
+			return {
+				...state,
+				status: action.payload ? "too_many_requests" : "error"
+			};
+		}
+
+		case "UPDATE_KEYWORDS": {
+			const { payload } = action;
+
+			return {
+				...state,
+				keywords: payload,
+				status:
+					payload.length === 0
+						? "characters_0"
+						: payload.length === 1
+						? "characters_1"
+						: payload.length === 2
+						? "characters_2"
+						: state.status
+			};
+		}
+
 		default:
 			return state;
 	}
