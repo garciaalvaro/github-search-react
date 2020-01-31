bb5e66cd9a801053ddc15f91f3f1c1aba387846d
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-01-31 19:12:29 +0900

    Added Language component

---
+++ src/components/Language/Language.tsx
@@ -0,0 +1,37 @@
+import React, { FunctionComponent } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
+import { className } from "../../utils";
+import { isLanguageActive, updateLanguage } from "../../redux";
+
+interface Props {
+	language: Language;
+}
+
+/**
+ * Language component
+ */
+export const Language: FunctionComponent<Props> = props => {
+	const { language } = props;
+
+	const is_active = useSelector<State, boolean>(state =>
+		isLanguageActive(state, language)
+	);
+
+	const dispatch = useDispatch();
+
+	return (
+		<button
+			key={language}
+			onClick={() => dispatch(updateLanguage(language))}
+			data-language={language}
+			className={className([
+				"btn",
+				"btn--underlined",
+				is_active ? "btn--is-active" : null
+			])}
+		>
+			{language}
+		</button>
+	);
+};
