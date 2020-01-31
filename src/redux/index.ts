11fb204621c81f063ca25342a4c10d88d393a084
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-01-31 18:04:45 +0900

    Included actions and selectors in redux index file

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,1 +1,7 @@
 export { store } from "./store";
+export { getKeywords, getStatus } from "./selectors";
+export {
+	updateKeywords,
+	fetchRepositories,
+	fetchRepositoriesFailed
+} from "./actions";
