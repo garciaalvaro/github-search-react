17a06fddcfb52db10cd1365c13b510aa8d7ebab3
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-01-31 18:00:30 +0900

    Updated FETCH_REPOSITORIES_COMPLETED to receive the "cleaned" repositories

--- src/redux/reducers/repositories.ts
+++ src/redux/reducers/repositories.ts
@@ -1,23 +1,13 @@
-import { prepareRepository } from "../utils/prepareRepository";
-
 const initial_state: StateRepositories = {};
 
 export const reducer = (
 	state = initial_state,
-	action: Actions | ActionsWithPayload
+	action: Actions
 ): StateRepositories => {
 	switch (action.type) {
 		// After repositories are fetched we add them to the store
 		case "FETCH_REPOSITORIES_COMPLETED": {
-			const repositories_fetched = action.payload.reduce(
-				(acc, repository) => ({
-					...acc,
-					[repository.id]: prepareRepository(repository)
-				}),
-				{}
-			);
-
-			return { ...state, ...repositories_fetched };
+			return { ...state, ...action.payload };
 		}
 
 		default:
