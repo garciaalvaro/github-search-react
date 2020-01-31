7f74274bd2c0a6469fcf9c29809a604e47734ee0
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-01-31 17:55:43 +0900

    Added types for redux states

---
+++ types/redux-state.d.ts
@@ -0,0 +1,27 @@
+type StateRepositories = Record<Repository["id"], Repository>;
+
+interface StateUi {
+	repositories_found: number;
+	repositories_ids: Repository["id"][];
+	keywords: string;
+	language: null | Language;
+	last_update: null | LastUpdate;
+	min_stars: null | MinStars;
+	page: number;
+	status:
+		| null
+		| "characters_0"
+		| "characters_1"
+		| "characters_2"
+		| "waiting"
+		| "loading"
+		| "no_results"
+		| "too_many_requests"
+		| "error";
+}
+
+// Global State
+interface State {
+	repositories: StateRepositories;
+	ui: StateUi;
+}
