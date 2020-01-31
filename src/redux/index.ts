8c64b2711831ae58905e8768e1d63b3870188726
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-01-31 18:16:24 +0900

    Included getLanguage redux selector

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,5 +1,5 @@
 export { store } from "./store";
-export { getKeywords, getStatus } from "./selectors";
+export { getKeywords, getStatus, getLanguage } from "./selectors";
 export {
 	updateKeywords,
 	fetchRepositories,
--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -2,3 +2,6 @@ export const getKeywords: Selector<StateUi["keywords"]> = state =>
 	state.ui.keywords;
 
 export const getStatus: Selector<StateUi["status"]> = state => state.ui.status;
+
+export const getLanguage: Selector<StateUi["language"]> = state =>
+	state.ui.language;
