4abc50a55f02ca36a19cf5bf0250872bfa4adeba
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-01-31 18:04:21 +0900

    Added index file for utils

---
+++ src/utils/index.ts
@@ -0,0 +1,6 @@
+export { encode } from "./encode";
+export { getTimeSince } from "./getTimeSince";
+export { IconSearch, IconStar } from "./icons";
+export { prepareRepository } from "./prepareRepository";
+export { getPrevDateFrom } from "./getPrevDateFrom";
+export { getUrl } from "./getUrl";
