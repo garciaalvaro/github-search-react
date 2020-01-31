44462d35615e82ec9abdaefafc19de0dbccf7a95
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-01-31 17:56:25 +0900

    Added types for main elements

---
+++ types/other.d.ts
@@ -0,0 +1,50 @@
+// Repository received from the API
+interface RepositoryRaw {
+	id: string;
+	html_url: string;
+	description: string;
+	name: string;
+	owner: {
+		login: string;
+	};
+	stargazers_count: number;
+	language: string;
+	updated_at: string;
+	license: null | {
+		name: string;
+	};
+}
+
+interface Repository {
+	id: string;
+	url: string;
+	description: string;
+	name: string;
+	user: string;
+	stars: number;
+	language: string;
+	updated: string;
+	license: string;
+}
+
+type MinStars = "10" | "100" | "1000" | "10000" | "50000" | "100000";
+
+type LastUpdate =
+	| "last_week"
+	| "last_month"
+	| "last_6_months"
+	| "last_year"
+	| "last_2_years"
+	| "last_3_years";
+
+type Language =
+	| "JavaScript"
+	| "TypeScript"
+	| "HTML"
+	| "CSS"
+	| "Objective-C"
+	| "Java"
+	| "Ruby"
+	| "Python"
+	| "PHP"
+	| "C#";
