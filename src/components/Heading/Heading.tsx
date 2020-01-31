d0a902c4dbd2b3266a7a7c18a77f8c5f1a160356
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-01-31 18:06:27 +0900

    Added Heading component

---
+++ src/components/Heading/Heading.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from "react";
+
+/**
+ * Heading component
+ */
+export const Heading: FunctionComponent = () => (
+	<h1>
+		<a href="https://github.com/garciaalvaro/github-search">
+			GitHub Search
+		</a>
+	</h1>
+);
