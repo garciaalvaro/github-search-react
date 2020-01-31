ab07a2f149fd16faf7f36ce2d4f2b27d6f3cef16
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-01-31 18:06:10 +0900

    Included middlewares to createStore

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,24 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import { reducers } from "./reducers";
+import {
+	throttle,
+	fetchRepositories,
+	logger,
+	minCharacters
+} from "./middlewares";
 
 export const store = createStore(
 	combineReducers(reducers),
-	composeWithDevTools()
+
+	composeWithDevTools(
+		applyMiddleware(
+			minCharacters,
+			throttle,
+			fetchRepositories,
+			minCharacters,
+			logger
+		)
+	)
 );
