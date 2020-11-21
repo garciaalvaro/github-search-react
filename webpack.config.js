67bf3319b325971df60d377391cf470ff5c7ada0
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-11-21 15:31:57 +0100

    Use a function for Webpack config, fixed post-css config and set the watch property

--- webpack.config.js
+++ webpack.config.js
@@ -4,91 +4,99 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
-const postcssPresetEnv = require("postcss-preset-env");
 const path = require("path");
 
-module.exports = {
-	entry: path.join(__dirname, "src/entry.ts"),
+module.exports = (env, { mode }) => {
+	const is_development = mode === "development";
 
-	output: {
-		path: path.join(__dirname, "dist"),
-		filename: "bundle.js",
-	},
+	return {
+		watch: is_development,
 
-	resolve: {
-		alias: {
-			"@": path.join(__dirname, "src"),
+		entry: path.join(__dirname, "src/entry.ts"),
+
+		output: {
+			path: path.join(__dirname, "dist"),
+			filename: "bundle.js",
 		},
-	},
 
-	module: {
-		rules: [
-			// TypeScript/JavaScript files
-			{
-				test: /\.tsx?$/,
-				exclude: /node_modules/,
-				loader: "babel-loader",
-				resolve: {
-					extensions: [".tsx", ".ts", ".js", ".jsx"],
-				},
+		resolve: {
+			alias: {
+				"@": path.join(__dirname, "src"),
 			},
+		},
 
-			// Stylus/CSS files
-			{
-				test: /\.(css|styl)$/,
-				use: [
-					MiniCssExtractPlugin.loader,
+		module: {
+			rules: [
+				// TypeScript/JavaScript files
+				{
+					test: /\.tsx?$/,
+					exclude: /node_modules/,
+					loader: "babel-loader",
+					resolve: {
+						extensions: [".tsx", ".ts", ".js", ".jsx"],
+					},
+				},
 
-					"css-loader",
+				// Stylus/CSS files
+				{
+					test: /\.(css|styl)$/,
+					use: [
+						MiniCssExtractPlugin.loader,
 
-					{
-						loader: "postcss-loader",
-						options: {
-							postcssOptions: {
-								plugins: () => [
-									postcssPresetEnv({
-										autoprefixer: { grid: true },
-									}),
-								],
+						"css-loader",
+
+						{
+							loader: "postcss-loader",
+							options: {
+								postcssOptions: {
+									plugins: [
+										[
+											"postcss-preset-env",
+											{
+												autoprefixer: { grid: true },
+											},
+										],
+									],
+								},
 							},
 						},
-					},
 
-					"stylus-loader",
-				],
-			},
+						"stylus-loader",
+					],
+				},
+			],
+		},
+
+		plugins: [
+			// Extract CSS into its own file
+			new MiniCssExtractPlugin({
+				filename: "bundle.css",
+			}),
+
+			// Add a banner with the project name and version
+			new BannerPlugin({
+				banner: `${name} | ${version} | ${license}`,
+				include: ["bundle.js", "bundle.css"],
+			}),
+
+			// This plugin will include the webpack produced script and style
+			// in the HTML file. It adds a hash to the url, which ensures the
+			// client downloads the latest version, in case there is a cached one.
+			new HtmlWebpackPlugin({
+				template: path.join(__dirname, "src/assets/index.html"),
+				hash: true,
+			}),
 		],
-	},
 
-	plugins: [
-		// Extract CSS into its own file
-		new MiniCssExtractPlugin({
-			filename: "bundle.css",
-		}),
+		optimization: {
+			minimize: true,
+			minimizer: [
+				new CssMinimizerPlugin(),
 
-		// Add a banner with the project name and version
-		new BannerPlugin({
-			banner: `${name} | ${version} | ${license}`,
-			include: ["bundle.js", "bundle.css"],
-		}),
-
-		// This plugin will include the webpack produced script and style
-		// in the HTML file. It adds a hash to the url, which ensures the
-		// client downloads the latest version, in case there is a cached one.
-		new HtmlWebpackPlugin({
-			template: path.join(__dirname, "src/assets/index.html"),
-			hash: true,
-		}),
-	],
-
-	optimization: {
-		minimize: true,
-		minimizer: [
-			new CssMinimizerPlugin(),
-
-			// As we are using a custom optimization, making use of
-			// CssMinimizerPlugin, we also need to specify TerserPlugin
-			new TerserPlugin({ extractComments: false }),
-		],
-	},
+				// As we are using a custom optimization, making use of
+				// CssMinimizerPlugin, we also need to specify TerserPlugin
+				new TerserPlugin({ extractComments: false }),
+			],
+		},
+	};
 };
