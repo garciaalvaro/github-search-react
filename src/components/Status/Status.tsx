b28f0fca7f4832a630876d7f78e19ace522583b1
Alvaro <4710040+garciaalvaro@users.noreply.github.com>
2020-11-21 15:30:19 +0100

    Extracted getTextFromStatus util to its own file

--- src/components/Status/Status.tsx
+++ src/components/Status/Status.tsx
@@ -2,44 +2,8 @@ import React, { FunctionComponent, useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import { getStatus } from "@/redux";
+import { getTextFromStatus } from "./getTextFromStatus";
 import { Message } from "../Message";
-
-export const getTextFromStatus = (status: StateUi["status"]) => {
-	switch (status) {
-		case "characters_0":
-			return "Enter some text in the search field above";
-
-		case "characters_1":
-			return "Enter 2 more characters";
-
-		case "characters_2":
-			return "Enter 1 more character";
-
-		case "too_many_requests":
-			return (
-				"It looks like too many requests were made. " +
-				"Please try again in a minute."
-			);
-
-		case "error":
-			return (
-				"It looks like there was an error getting the data. " +
-				"Please try again in some time."
-			);
-
-		case "waiting":
-			return "Waiting...";
-
-		case "loading":
-			return "Loading...";
-
-		case "no_results":
-			return "No results";
-
-		default:
-			return "";
-	}
-};
 
 /**
  * Status component
